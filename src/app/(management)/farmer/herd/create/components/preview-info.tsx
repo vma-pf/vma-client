e17599c589d1ca8a0357f3408edea1142d4322d6
dat <tranminhdat0805@gmail.com>
2024-09-26 19:47:33 +0700

    feat: add preview herd info screen

--- src/app/(management)/farmer/herd/create/components/preview-info.tsx
+++ src/app/(management)/farmer/herd/create/components/preview-info.tsx
@@ -14,6 +14,37 @@ import {
 } from "@nextui-org/react";
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import { Pig } from "./assign-tag";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@oursrc/components/ui/hover-card";
+
+const assignedPigs: Pig[] = [
+  { id: 1, name: "Heo 001", cage: { id: "1", name: "Chuồng 001", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 2, name: "Heo 002", cage: { id: "1", name: "Chuồng 001", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 3, name: "Heo 003", cage: { id: "1", name: "Chuồng 001", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 4, name: "Heo 004", cage: { id: "2", name: "Chuồng 002", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 5, name: "Heo 005", cage: { id: "2", name: "Chuồng 002", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 6, name: "Heo 006", cage: { id: "2", name: "Chuồng 002", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 7, name: "Heo 007", cage: { id: "2", name: "Chuồng 002", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 8, name: "Heo 008", cage: { id: "3", name: "Chuồng 003", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 9, name: "Heo 009", cage: { id: "3", name: "Chuồng 003", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 10, name: "Heo 010", cage: { id: "3", name: "Chuồng 003", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 11, name: "Heo 011", cage: { id: "4", name: "Chuồng 004", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 12, name: "Heo 012", cage: { id: "4", name: "Chuồng 004", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 13, name: "Heo 013", cage: { id: "4", name: "Chuồng 004", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 14, name: "Heo 014", cage: { id: "5", name: "Chuồng 005", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 15, name: "Heo 015", cage: { id: "5", name: "Chuồng 005", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 16, name: "Heo 016", cage: { id: "5", name: "Chuồng 005", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 17, name: "Heo 017", cage: { id: "5", name: "Chuồng 005", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 18, name: "Heo 018", cage: { id: "6", name: "Chuồng 006", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 19, name: "Heo 019", cage: { id: "6", name: "Chuồng 006", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 20, name: "Heo 020", cage: { id: "6", name: "Chuồng 006", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 21, name: "Heo 021", cage: { id: "6", name: "Chuồng 006", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 22, name: "Heo 022", cage: { id: "6", name: "Chuồng 006", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 23, name: "Heo 023", cage: { id: "6", name: "Chuồng 006", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 24, name: "Heo 024", cage: { id: "6", name: "Chuồng 006", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 25, name: "Heo 025", cage: { id: "6", name: "Chuồng 006", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+  { id: 26, name: "Heo 026", cage: { id: "6", name: "Chuồng 006", capacity: 10, currentQuantity: 10 }, weight: 100, height: 100, width: 100 },
+];
 
 const PreviewInfo = () => {
   const {
@@ -67,7 +98,7 @@ const PreviewInfo = () => {
     });
   };
 
-  const handleSubmitForm = async (data: any) => {};
+  const handleSubmitForm = async (data: any) => { };
   return (
     <div className="container mx-auto mt-12 mb-8">
       <h1 className="text-3xl">Preview thông tin</h1>
@@ -212,7 +243,173 @@ const PreviewInfo = () => {
             </AccordionItem>
           </Accordion>
         </form>
-        <p className="text-center mb-5">Danh sách heo</p>
+      </div>
+      <div className="mt-5 rounded-2xl bg-white dark:bg-zinc-800 shadow-lg">
+        <p className="text-3xl font-bold text-center mb-5">Danh sách heo</p>
+        <div className="mt-2 grid grid-cols-3">
+          <div className="px-3 m-2 rounded-2xl bg-white dark:bg-zinc-800 shadow-lg">
+            <p className="text-center text-xl font-semibold">Chuồng 001</p>
+            <p className="text-center text-lg">Sức chứa: {
+              assignedPigs.find((pig) => pig.cage?.id === "1")?.cage?.currentQuantity
+            }</p>
+            <div className="grid grid-cols-3">
+              {assignedPigs
+                .filter((pig) => pig.cage?.id === "1")
+                .map((pig: Pig, index) => (
+                  <div
+                    className="mx-2 my-3 p-2 flex flex-col justify-center items-center border-2 rounded-xl shadow-md cursor-pointer"
+                    key={index}
+                  >
+                    <HoverCard>
+                      <HoverCardTrigger asChild>
+                        <p className="text-lg font-semibold">{pig.name}</p>
+                      </HoverCardTrigger>
+                      <HoverCardContent>
+                        <p className="text-lg">Cân nặng: {pig.weight}</p>
+                        <p className="text-lg">Chiều cao: {pig.height}</p>
+                        <p className="text-lg">Chiều rộng: {pig.width}</p>
+                      </HoverCardContent>
+                    </HoverCard>
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="px-3 m-2 rounded-2xl bg-white dark:bg-zinc-800 shadow-lg">
+            <p className="text-center text-xl font-semibold">Chuồng 002</p>
+            <p className="text-center text-lg">Sức chứa: {
+              assignedPigs.find((pig) => pig.cage?.id === "2")?.cage?.currentQuantity
+            }</p>
+            <div className="grid grid-cols-3">
+              {assignedPigs
+                .filter((pig) => pig.cage?.id === "2")
+                .map((pig: Pig, index) => (
+                  <div
+                    className="mx-2 my-3 p-2 flex flex-col justify-center items-center border-2 rounded-xl shadow-md cursor-pointer"
+                    key={index}
+                  >
+                    <HoverCard>
+                      <HoverCardTrigger asChild>
+                        <p className="text-lg font-semibold">{pig.name}</p>
+                      </HoverCardTrigger>
+                      <HoverCardContent>
+                        <p className="text-lg">Cân nặng: {pig.weight}</p>
+                        <p className="text-lg">Chiều cao: {pig.height}</p>
+                        <p className="text-lg">Chiều rộng: {pig.width}</p>
+                      </HoverCardContent>
+                    </HoverCard>
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="px-3 m-2 rounded-2xl bg-white dark:bg-zinc-800 shadow-lg">
+            <p className="text-center text-xl font-semibold">Chuồng 003</p>
+            <p className="text-center text-lg">Sức chứa: {
+              assignedPigs.find((pig) => pig.cage?.id === "3")?.cage?.currentQuantity
+            }</p>
+            <div className="grid grid-cols-3">
+              {assignedPigs
+                .filter((pig) => pig.cage?.id === "3")
+                .map((pig: Pig, index) => (
+                  <div
+                    className="mx-2 my-3 p-2 flex flex-col justify-center items-center border-2 rounded-xl shadow-md cursor-pointer"
+                    key={index}
+                  >
+                    <HoverCard>
+                      <HoverCardTrigger asChild>
+                        <p className="text-lg font-semibold">{pig.name}</p>
+                      </HoverCardTrigger>
+                      <HoverCardContent>
+                        <p className="text-lg">Cân nặng: {pig.weight}</p>
+                        <p className="text-lg">Chiều cao: {pig.height}</p>
+                        <p className="text-lg">Chiều rộng: {pig.width}</p>
+                      </HoverCardContent>
+                    </HoverCard>
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="px-3 m-2 rounded-2xl bg-white dark:bg-zinc-800 shadow-lg">
+            <p className="text-center text-xl font-semibold">Chuồng 004</p>
+            <p className="text-center text-lg">Sức chứa: {
+              assignedPigs.find((pig) => pig.cage?.id === "4")?.cage?.currentQuantity
+            }</p>
+            <div className="grid grid-cols-3">
+              {assignedPigs
+                .filter((pig) => pig.cage?.id === "4")
+                .map((pig: Pig, index) => (
+                  <div
+                    className="mx-2 my-3 p-2 flex flex-col justify-center items-center border-2 rounded-xl shadow-md cursor-pointer"
+                    key={index}
+                  >
+                    <HoverCard>
+                      <HoverCardTrigger asChild>
+                        <p className="text-lg font-semibold">{pig.name}</p>
+                      </HoverCardTrigger>
+                      <HoverCardContent>
+                        <p className="text-lg">Cân nặng: {pig.weight}</p>
+                        <p className="text-lg">Chiều cao: {pig.height}</p>
+                        <p className="text-lg">Chiều rộng: {pig.width}</p>
+                      </HoverCardContent>
+                    </HoverCard>
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="px-3 m-2 rounded-2xl bg-white dark:bg-zinc-800 shadow-lg">
+            <p className="text-center text-xl font-semibold">Chuồng 005</p>
+            <p className="text-center text-lg">Sức chứa: {
+              assignedPigs.find((pig) => pig.cage?.id === "5")?.cage?.currentQuantity
+            }</p>
+            <div className="grid grid-cols-3">
+              {assignedPigs
+                .filter((pig) => pig.cage?.id === "5")
+                .map((pig: Pig, index) => (
+                  <div
+                    className="mx-2 my-3 p-2 flex flex-col justify-center items-center border-2 rounded-xl shadow-md cursor-pointer"
+                    key={index}
+                  >
+                    <HoverCard>
+                      <HoverCardTrigger asChild>
+                        <p className="text-lg font-semibold">{pig.name}</p>
+                      </HoverCardTrigger>
+                      <HoverCardContent>
+                        <p className="text-lg">Cân nặng: {pig.weight}</p>
+                        <p className="text-lg">Chiều cao: {pig.height}</p>
+                        <p className="text-lg">Chiều rộng: {pig.width}</p>
+                      </HoverCardContent>
+                    </HoverCard>
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="px-3 m-2 rounded-2xl bg-white dark:bg-zinc-800 shadow-lg">
+            <p className="text-center text-xl font-semibold">Chuồng 006</p>
+            <p className="text-center text-lg">Sức chứa: {
+              assignedPigs.find((pig) => pig.cage?.id === "6")?.cage?.currentQuantity
+            }</p>
+            <div className="grid grid-cols-3">
+              {assignedPigs
+                .filter((pig) => pig.cage?.id === "6")
+                .map((pig: Pig, index) => (
+                  <div
+                    className="mx-2 my-3 p-2 flex flex-col justify-center items-center border-2 rounded-xl shadow-md cursor-pointer"
+                    key={index}
+                  >
+                    <HoverCard>
+                      <HoverCardTrigger asChild>
+                        <p className="text-lg font-semibold">{pig.name}</p>
+                      </HoverCardTrigger>
+                      <HoverCardContent>
+                        <p className="text-lg">Cân nặng: {pig.weight}</p>
+                        <p className="text-lg">Chiều cao: {pig.height}</p>
+                        <p className="text-lg">Chiều rộng: {pig.width}</p>
+                      </HoverCardContent>
+                    </HoverCard>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
