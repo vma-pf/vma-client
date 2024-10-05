944811e2e46a919aa3e460e8c027a2db994b3c7b
nguyen.quang.huy <nguyen.quang.huy@vina-payroll.com>
2024-10-05 09:09:10 +0700

    feat: prepare cage in herd process

--- src/app/(management)/farmer/herd/create/_components/cage-create.tsx
+++ src/app/(management)/farmer/herd/create/_components/cage-create.tsx
@@ -9,12 +9,14 @@ import { useAppDispatch } from "@oursrc/lib/hooks";
 import { HiChevronDoubleRight } from "react-icons/hi2";
 import { ResponseObjectList } from "@oursrc/lib/models/response-object";
 import { cageService } from "@oursrc/lib/services/cageService";
-import { CreateCageRequest } from "@oursrc/lib/models/cage";
+import { Cage, CreateCageRequest } from "@oursrc/lib/models/cage";
+import PrepareCageList from "./prepare-cage-list";
 
 const CageCreate = () => {
   const dispatch = useAppDispatch();
   const [loading, setLoading] = React.useState<boolean | undefined>(false);
   const [capacity, setCapacity] = React.useState<string>();
+  const [newCages, setNewCages] = React.useState<Cage[]>([]);
   const {
     register,
     handleSubmit,
@@ -39,9 +41,17 @@ const CageCreate = () => {
       if (res && res.isSuccess) {
         toast({
           variant: "success",
-          title: res.data.toString(),
+          title: 'Tạo mới chuồng thành công',
         });
-        dispatch(setNextHerdProgressStep());
+        setNewCages([])
+        setNewCages([...newCages, {
+          id: "",
+          code: data.code,
+          capacity: data.capacity,
+          description: data.description,
+          availableQuantity: 0
+        }])
+        // dispatch(setNextHerdProgressStep());
       } else {
         toast({
           variant: "destructive",
@@ -71,95 +81,102 @@ const CageCreate = () => {
   return (
     <div>
       <div className="container mx-auto">
-        <div className="mt-12 mb-6 flex justify-between">
-          <h1 className="text-3xl">
-            Tạo chuồng mới
-            <span className="ml-3 text-base text-gray-400">(Optional)</span>
-          </h1>
-          <Button variant="ghost" color="primary" endContent={<HiChevronDoubleRight size={20} />} onPress={() => dispatch(setNextHerdProgressStep())}>
-            Bỏ qua bước này
-          </Button>
-        </div>
-        <div className="w-100">
-          <AttachMedia size="1/2" />
-        </div>
-        <div className="mt-12">
-          <h1 className="text-xl">Thông tin chuồng</h1>
-        </div>
-        <form onSubmit={handleSubmit(handleSubmitForm)}>
-          <div className="grid grid-flow-row grid-cols-2 gap-4 mt-10">
-            <div className="flex w-full flex-wrap md:flex-nowrap">
-              <Input
-                className="mb-5"
-                type="text"
-                radius="sm"
-                size="lg"
-                label="Mã chuồng"
-                placeholder="Nhập mã chuồng"
-                labelPlacement="outside"
-                isRequired
-                isInvalid={errors.code ? true : false}
-                errorMessage="Mã chuồng không được để trống"
-                {...register("code", { required: true })}
-              />
+        <div className="grid grid-cols-2 gap-4">
+          <div className="mt-12 ">
+            <h1 className="text-3xl mb-8">
+              Chuồng có sẵn
+            </h1>
+            <PrepareCageList hasNewCages={newCages.length > 0} />
+          </div>
+          <div>
+            <div className="mt-12 mb-6 flex justify-between">
+              <h1 className="text-3xl">
+                Tạo chuồng mới
+                <span className="ml-3 text-base text-gray-400">(Optional)</span>
+              </h1>
+              <Button
+                variant="ghost"
+                color="primary"
+                endContent={<HiChevronDoubleRight size={20} />}
+                onPress={() => dispatch(setNextHerdProgressStep())}
+              >
+                Bỏ qua bước này
+              </Button>
             </div>
-            <div className="flex w-full flex-wrap md:flex-nowrap">
-              <Input
-                className="mb-5"
-                type="text"
-                radius="sm"
-                size="lg"
-                label="Sức chứa"
-                placeholder="Nhập sức chứa"
-                labelPlacement="outside"
-                isRequired
-                isInvalid={errors.capacity ? true : false}
-                errorMessage="Sức chứa không được để trống"
-                value={capacity || ""}
-                onValueChange={(event) => handleNumberChange(event)}
-                {...register("capacity", {
-                  required: true,
-                  valueAsNumber: true,
-                })}
-              />
+            {/* <div className="w-100">
+              <AttachMedia />
+            </div> */}
+            <div className="mt-12">
+              <h1 className="text-xl">Thông tin chuồng</h1>
             </div>
+            <form onSubmit={handleSubmit(handleSubmitForm)}>
+              <div className="grid grid-flow-row grid-cols-2 gap-4 mt-10">
+                <div className="flex w-full flex-wrap md:flex-nowrap">
+                  <Input
+                    className="mb-5"
+                    type="text"
+                    radius="sm"
+                    size="lg"
+                    label="Mã chuồng"
+                    placeholder="Nhập mã chuồng"
+                    labelPlacement="outside"
+                    isRequired
+                    isInvalid={errors.code ? true : false}
+                    errorMessage="Mã chuồng không được để trống"
+                    {...register("code", { required: true })}
+                  />
+                </div>
+                <div className="flex w-full flex-wrap md:flex-nowrap">
+                  <Input
+                    className="mb-5"
+                    type="text"
+                    radius="sm"
+                    size="lg"
+                    label="Sức chứa"
+                    placeholder="Nhập sức chứa"
+                    labelPlacement="outside"
+                    isRequired
+                    isInvalid={errors.capacity ? true : false}
+                    errorMessage="Sức chứa không được để trống"
+                    value={capacity || ""}
+                    onValueChange={(event) => handleNumberChange(event)}
+                    {...register("capacity", {
+                      required: true,
+                      valueAsNumber: true,
+                    })}
+                  />
+                </div>
+              </div>
+              <div className="grid grid-cols-1 mb-5">
+                <Textarea
+                  minRows={20}
+                  type="text"
+                  radius="sm"
+                  size="lg"
+                  label="Mô tả"
+                  placeholder="Nhập mô tả"
+                  labelPlacement="outside"
+                  isRequired
+                  isInvalid={errors.description ? true : false}
+                  errorMessage="Mô tả không được để trống"
+                  {...register("description", { required: true })}
+                />
+              </div>
+              <div className="flex justify-end">
+                <Button
+                  variant="solid"
+                  color="primary"
+                  isDisabled={errors && Object.keys(errors).length > 0}
+                  isLoading={loading}
+                  size="lg"
+                  type="submit"
+                >
+                  <p className="text-white">Tạo mới</p>
+                </Button>
+              </div>
+            </form>
           </div>
-          {/* <div className="grid grid-flow-row grid-cols-1 gap-4">
-            <Input
-              className="mb-5"
-              type="text"
-              radius="sm"
-              size="lg"
-              label="Vị trí"
-              placeholder="Nhập vị trí"
-              labelPlacement="outside"
-              isRequired
-              isInvalid={errors.position ? true : false}
-              errorMessage="Vị trí không được để trống"
-              {...register("position", { required: true })}
-            />
-          </div> */}
-          <div className="grid grid-cols-1 mb-5">
-            <Textarea
-              minRows={20}
-              type="text"
-              radius="sm"
-              size="lg"
-              label="Mô tả"
-              placeholder="Nhập mô tả"
-              labelPlacement="outside"
-              isRequired
-              isInvalid={errors.description ? true : false}
-              errorMessage="Mô tả không được để trống"
-              {...register("description", { required: true })}
-            />
-          </div>
-          <div className="flex justify-end">
-            <Button variant="solid" color="primary" isDisabled={errors && Object.keys(errors).length > 0} isLoading={loading} size="lg" type="submit">
-              <p className="text-white">Bước tiếp theo</p>
-            </Button>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
---
+++ src/app/(management)/farmer/herd/create/_components/prepare-cage-list.tsx
@@ -0,0 +1,342 @@
+"use client";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Input,
+  Pagination,
+  Selection,
+  SortDescriptor,
+  Spinner,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  Tooltip,
+} from "@nextui-org/react";
+import {
+  columns,
+  INITIAL_VISIBLE_COLUMNS,
+  statusOptions,
+} from "@oursrc/components/cages/models/cage-table-data";
+import { capitalize } from "@oursrc/components/utils";
+import { useToast } from "@oursrc/hooks/use-toast";
+import { Cage } from "@oursrc/lib/models/cage";
+import { cageService } from "@oursrc/lib/services/cageService";
+import { Search } from "lucide-react";
+import React from "react";
+import { HiChevronDown } from "react-icons/hi";
+const PrepareCageList = ({ hasNewCages }: any) => {
+  const { toast } = useToast();
+
+  //Table field
+  const [filterValue, setFilterValue] = React.useState("");
+  const [selectedKeys, setSelectedKeys] = React.useState<Selection>(
+    new Set([])
+  );
+  const [visibleColumns, setVisibleColumns] = React.useState<Selection>(
+    new Set(INITIAL_VISIBLE_COLUMNS)
+  );
+  const [statusFilter, setStatusFilter] = React.useState<Selection>("all");
+  const [totalRecords, setTotalRecords] = React.useState(0);
+  const [totalPages, setTotalPages] = React.useState(1);
+  const [rowsPerPage, setRowsPerPage] = React.useState(30);
+  const [sortDescriptor, setSortDescriptor] = React.useState<SortDescriptor>({
+    column: "lastUpdatedAt",
+    direction: "ascending",
+  });
+
+  const [page, setPage] = React.useState(1);
+  const hasSearchFilter = Boolean(filterValue);
+
+  const headerColumns = React.useMemo(() => {
+    if (visibleColumns === "all") return columns;
+
+    return columns.filter((column: any) =>
+      Array.from(visibleColumns).includes(column.uid)
+    );
+  }, [visibleColumns]);
+  const [cageList, setCageList] = React.useState<Cage[]>([]);
+
+  const [loading, setLoading] = React.useState(false);
+  const loadingState = loading || cageList?.length === 0 ? "loading" : "idle";
+
+  //Use Effect
+  React.useEffect(() => {
+    // setSelected(
+    //   cageList.filter((x) => Array.from(selectedKeys).includes(x.id))
+    // );
+  }, [selectedKeys]);
+
+  React.useEffect(() => {
+    if (hasNewCages) {
+      fetchData();
+    }
+  }, [hasNewCages]);
+
+  React.useEffect(() => {
+    fetchData();
+  }, [page, rowsPerPage]);
+
+  //API function
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await cageService.getCages(page, rowsPerPage);
+      if (response.isSuccess) {
+        setCageList(response.data.data);
+        setRowsPerPage(response.data.pageSize);
+        setTotalPages(response.data.totalPages);
+        setTotalRecords(response.data.totalRecords);
+      } else {
+        throw new AggregateError(response.errorMessage);
+      }
+    } catch (e) {
+      toast({
+        variant: "destructive",
+        title:
+          e instanceof AggregateError
+            ? e.message
+            : "Lỗi hệ thống. Vui lòng thử lại sau!",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const filteredItems = React.useMemo(() => {
+    let cloneFilteredItems: Cage[] = [...cageList];
+
+    if (hasSearchFilter) {
+      cloneFilteredItems = cloneFilteredItems.filter((item) =>
+        item.code.toLowerCase().includes(filterValue.toLowerCase())
+      );
+    }
+    if (
+      statusFilter !== "all" &&
+      Array.from(statusFilter).length !== statusOptions.length
+    ) {
+      cloneFilteredItems = cloneFilteredItems.filter((item) =>
+        Array.from(statusFilter).includes(item.code as string)
+      );
+    }
+    return cloneFilteredItems;
+  }, [cageList, filterValue, statusFilter]);
+
+  const items = React.useMemo(() => {
+    return filteredItems;
+  }, [filteredItems]);
+
+  const sortedItems = React.useMemo(() => {
+    return [...items].sort((a: Cage, b: Cage) => {
+      const first = a[sortDescriptor.column as keyof Cage] as number;
+      const second = b[sortDescriptor.column as keyof Cage] as number;
+      const cmp = first < second ? -1 : first > second ? 1 : 0;
+
+      return sortDescriptor.direction === "descending" ? -cmp : cmp;
+    });
+  }, [sortDescriptor, items]);
+
+  //call api
+  const onRowsPerPageChange = React.useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setRowsPerPage(Number(e.target.value));
+      setPage(1);
+    },
+    []
+  );
+  //call api
+  const onSearchChange = React.useCallback((value?: string) => {
+    if (value) {
+      setFilterValue(value);
+      setPage(1);
+    } else {
+      setFilterValue("");
+    }
+  }, []);
+
+  const onClear = React.useCallback(() => {
+    setFilterValue("");
+    setPage(1);
+  }, []);
+  const topContent = React.useMemo(() => {
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between gap-3 items-end">
+          <Input
+            isClearable
+            className="w-full sm:max-w-[44%]"
+            placeholder="Tìm kiếm theo mã..."
+            startContent={<Search />}
+            value={filterValue}
+            onClear={() => onClear()}
+            onValueChange={onSearchChange}
+          />
+          <div className="flex gap-3">
+            <Dropdown>
+              <DropdownTrigger className="hidden sm:flex">
+                <Button
+                  endContent={<HiChevronDown className="text-small" />}
+                  variant="flat"
+                >
+                  Hiển thị cột
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                disallowEmptySelection
+                aria-label="Table Columns"
+                closeOnSelect={false}
+                selectedKeys={visibleColumns}
+                selectionMode="multiple"
+                onSelectionChange={setVisibleColumns}
+              >
+                {columns.map((column) => (
+                  <DropdownItem key={column.uid} className="capitalize">
+                    {capitalize(column.name)}
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+            {/* <Button color="primary" endContent={<Plus />} onPress={onOpen}>
+              Tạo mới
+            </Button> */}
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <span className="text-default-400 text-small">
+            Tổng cộng {totalRecords} kết quả
+          </span>
+          <label className="flex items-center text-default-400 text-small">
+            Số hàng mỗi trang:
+            <select
+              className="bg-transparent outline-none text-default-400 text-small"
+              onChange={onRowsPerPageChange}
+            >
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+            </select>
+          </label>
+        </div>
+      </div>
+    );
+  }, [
+    filterValue,
+    statusFilter,
+    visibleColumns,
+    onSearchChange,
+    onRowsPerPageChange,
+    cageList.length,
+    hasSearchFilter,
+  ]);
+  const renderCell = React.useCallback((data: Cage, columnKey: React.Key) => {
+    const cellValue = data[columnKey as keyof Cage];
+
+    switch (columnKey) {
+      case "description":
+        return (
+          <Tooltip
+            showArrow={true}
+            content={cellValue}
+            color="primary"
+            delay={1000}
+          >
+            <p className="truncate cursor-default">{cellValue}</p>
+          </Tooltip>
+        );
+      // case "actions":
+      //   return (
+      //     <div className="flex justify-end items-center gap-2">
+      //       <Tooltip content="Chi tiết">
+      //         <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+      //           <EyeIcon />
+      //         </span>
+      //       </Tooltip>
+      //       <Tooltip content="Chỉnh sửa">
+      //         <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+      //           <EditIcon onClick={() => onEdit(data)} />
+      //         </span>
+      //       </Tooltip>
+      //       <Tooltip color="danger" content="Xóa">
+      //         <span className="text-lg text-danger cursor-pointer active:opacity-50">
+      //           <Trash2Icon onClick={() => onDelete(data)} />
+      //         </span>
+      //       </Tooltip>
+      //     </div>
+      //   );
+      default:
+        return cellValue;
+    }
+  }, []);
+  const bottomContent = React.useMemo(() => {
+    return (
+      <div className="py-2 px-2 flex justify-between items-center">
+        <span className="w-[30%] text-small text-default-400">
+          {selectedKeys === "all"
+            ? "Đã chọn tất cả"
+            : `Đã chọn ${selectedKeys.size} kết quả`}
+        </span>
+        <Pagination
+          isCompact
+          showControls
+          showShadow
+          color="primary"
+          page={page}
+          total={totalPages}
+          onChange={setPage}
+        />
+        <div className="hidden sm:flex w-[30%] justify-end gap-2"></div>
+      </div>
+    );
+  }, [selectedKeys, items.length, page, totalPages, hasSearchFilter]);
+
+  return (
+    <div>
+      <Table
+        isStriped
+        color="primary"
+        bottomContent={bottomContent}
+        bottomContentPlacement="outside"
+        classNames={{
+          wrapper: "min-h-[500px]",
+        }}
+        sortDescriptor={sortDescriptor}
+        topContent={topContent}
+        topContentPlacement="outside"
+        onSelectionChange={setSelectedKeys}
+        onSortChange={setSortDescriptor}
+      >
+        <TableHeader columns={headerColumns}>
+          {(column: any) => (
+            <TableColumn
+              key={column.uid}
+              align={column.uid === "actions" ? "center" : "start"}
+              allowsSorting={column.sortable}
+            >
+              {column.name.toUpperCase()}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody
+          emptyContent={"Không có kết quả"}
+          items={sortedItems}
+          loadingContent={<Spinner />}
+          loadingState={loadingState}
+        >
+          {(item) => (
+            <TableRow key={item.id} className="h-12">
+              {(columnKey) => (
+                <TableCell>{renderCell(item, columnKey)}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+export default PrepareCageList;
