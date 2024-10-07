35004535fdab85b35d6aaa54d821c19035031228
nguyen.quang.huy <nguyen.quang.huy@vina-payroll.com>
2024-10-07 20:40:29 +0700

    enhance code

--- src/app/(management)/veterinarian/vaccination/create-plan/_components/first-vaccination-step.tsx
+++ src/app/(management)/veterinarian/vaccination/create-plan/_components/first-vaccination-step.tsx
@@ -32,7 +32,10 @@ import SelectedPigsList from "./selected-pigs-list";
 import { VaccinationStageProps } from "@oursrc/lib/models/vaccination";
 import { ResponseObjectList } from "@oursrc/lib/models/response-object";
 
-const FirstVaccinationStep = ({ setStep, setVaccinationPlanFirstStepResult }: any) => {
+const FirstVaccinationStep = ({
+  setStep,
+  setVaccinationPlanFirstStepResult,
+}: any) => {
   //State
   const [selectedCages, setSelectedCages] = React.useState<Cage[]>([]);
   const [selectedHerds, setSelectedHerds] = React.useState<Herd[]>([]);
@@ -41,7 +44,9 @@ const FirstVaccinationStep = ({ setStep, setVaccinationPlanFirstStepResult }: an
 
   const [stages, setStages] = React.useState<VaccinationStageProps[]>([
     {
-      id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+      id:
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15),
       title: "",
       timeSpan: "1",
       applyStageTime: "",
@@ -50,7 +55,9 @@ const FirstVaccinationStep = ({ setStep, setVaccinationPlanFirstStepResult }: an
   ]);
   const [date, setDate] = React.useState<RangeValue<CalendarDate>>({
     start: parseDate(new Date().toJSON().slice(0, 10)),
-    end: parseDate(new Date(new Date().getTime() + 86400000).toJSON().slice(0, 10)),
+    end: parseDate(
+      new Date(new Date().getTime() + 86400000).toJSON().slice(0, 10)
+    ),
   });
 
   const {
@@ -81,13 +88,18 @@ const FirstVaccinationStep = ({ setStep, setVaccinationPlanFirstStepResult }: an
     try {
       let fetchedPigs: Pig[] = [];
       if (fetchBy === "herd") {
-        const response: ResponseObjectList<Pig> = await pigService.getPigsByHerdId(selectedHerds[0]?.id ?? "", 1, 100);
+        const response: ResponseObjectList<Pig> =
+          await pigService.getPigsByHerdId(selectedHerds[0]?.id ?? "", 1, 100);
         if (response.isSuccess) {
           fetchedPigs = [...response.data.data, ...fetchedPigs];
         }
       } else {
         for (let i = 0; i < selectedCages.length; i++) {
-          const response = await pigService.getPigsByCageId(selectedCages[i]?.id ?? "", 1, 100);
+          const response = await pigService.getPigsByCageId(
+            selectedCages[i]?.id ?? "",
+            1,
+            100
+          );
           if (response.isSuccess) {
             fetchedPigs = [...response.data.data, ...fetchedPigs];
           } else {
@@ -103,7 +115,10 @@ const FirstVaccinationStep = ({ setStep, setVaccinationPlanFirstStepResult }: an
   };
 
   const checkStep1Completed = (): void => {
-    const validateStages = stages.filter((x: VaccinationStageProps) => x.title === "" || x.timeSpan === "" || x.applyStageTime === "");
+    const validateStages = stages.filter(
+      (x: VaccinationStageProps) =>
+        x.title === "" || x.timeSpan === "" || x.applyStageTime === ""
+    );
     if (validateStages.length > 0) {
       toast({
         variant: "destructive",
@@ -148,7 +163,8 @@ const FirstVaccinationStep = ({ setStep, setVaccinationPlanFirstStepResult }: an
         toast({
           variant: "success",
           title: "Tạo thành công bước 1",
-          description: "Đã tạo thành công lịch tiêm phòng bước 1! Vui lòng qua bước 2 để thêm thuốc cho giai đoạn",
+          description:
+            "Đã tạo thành công lịch tiêm phòng bước 1! Vui lòng qua bước 2 để thêm thuốc cho giai đoạn",
         });
         setStep(2);
         setVaccinationPlanFirstStepResult(response.data);
@@ -191,7 +207,9 @@ const FirstVaccinationStep = ({ setStep, setVaccinationPlanFirstStepResult }: an
   };
 
   const onAddStage = () => {
-    const newId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    const newId =
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15);
     setStages([
       ...stages,
       {
@@ -204,19 +222,29 @@ const FirstVaccinationStep = ({ setStep, setVaccinationPlanFirstStepResult }: an
     ]);
   };
   const onDeleteStage = (stage: VaccinationStageProps) => {
-    setStages([...stages.filter((x: VaccinationStageProps) => x.id !== stage.id)]);
+    setStages([
+      ...stages.filter((x: VaccinationStageProps) => x.id !== stage.id),
+    ]);
   };
   return (
     <div>
       <div className="container mx-auto">
         <form onSubmit={handleSubmit(handleSubmitForm)}>
           <div className="flex justify-end">
-            <Button color="primary" variant="solid" isDisabled={errors && Object.keys(errors).length > 0} type="submit" endContent={<ChevronRight />}>
+            <Button
+              color="primary"
+              variant="solid"
+              isDisabled={errors && Object.keys(errors).length > 0}
+              type="submit"
+              endContent={<ChevronRight />}
+            >
               Tiếp theo
             </Button>
           </div>
           <Card className="p-4 mt-4">
-            <p className="text-2xl mb-2 font-semibold">Thông tin kế hoạch tiêm phòng</p>
+            <p className="text-2xl mb-2 font-semibold">
+              Thông tin kế hoạch tiêm phòng
+            </p>
             <div className="grid grid-flow-row grid-cols-2 gap-4 mt-2">
               <div className="flex flex-col w-full flex-wrap md:flex-nowrap">
                 <Input
@@ -281,8 +309,12 @@ const FirstVaccinationStep = ({ setStep, setVaccinationPlanFirstStepResult }: an
           <Card className="my-4">
             <CardBody>
               <div className="mb-2 flex flex-row justify-between">
-                <p className="text-2xl font-semibold">Giai đoạn</p>
-                <Button color="primary" endContent={<Plus />} onClick={onAddStage}>
+                <p className="text-2xl font-semibold">Giai đoạn tiêm phòng</p>
+                <Button
+                  color="primary"
+                  endContent={<Plus />}
+                  onClick={onAddStage}
+                >
                   Thêm giai đoạn
                 </Button>
               </div>
@@ -290,56 +322,110 @@ const FirstVaccinationStep = ({ setStep, setVaccinationPlanFirstStepResult }: an
                 return (
                   <div key={stage.id} className="flex flex-row justify-between">
                     <div className="w-full grid grid-cols-3 gap-4">
-                      <Input
-                        className="mb-5"
-                        type="text"
-                        radius="sm"
-                        size="lg"
-                        label="Tên giai đoạn"
-                        placeholder="Nhập tên giai đoạn"
-                        labelPlacement="outside"
-                        isRequired
-                        value={stage.title}
-                        isInvalid={stage.title ? false : true}
-                        errorMessage="Tên giai đoạn không được để trống"
-                        onValueChange={(event) => onStageChange(event, "title", stage.id || "")}
-                      />
-                      <DatePicker
-                        className="mb-5"
-                        radius="sm"
-                        size="lg"
-                        label="Ngày tiêm"
-                        value={stage.applyStageTime ? parseDate(stage.applyStageTime) : undefined}
-                        isDateUnavailable={(date: DateValue) => stages.some((x: VaccinationStageProps) => x.applyStageTime === date.toString() && x.id !== stage.id)}
-                        minValue={date.start}
-                        maxValue={date.end}
-                        labelPlacement="outside"
-                        isRequired
-                        isInvalid={stage.applyStageTime ? false : true}
-                        errorMessage="Ngày tiêm không được để trống"
-                        onChange={(event) => onStageChange(event.toString(), "applyStageTime", stage.id || "")}
-                      />
-                      <Input
-                        className="mb-5"
-                        type="number"
-                        min={1}
-                        max={50}
-                        radius="sm"
-                        size="lg"
-                        label="Số ngày thực hiện (dự kiến)"
-                        placeholder="Nhập số ngày thực hiện (dự kiến)"
-                        labelPlacement="outside"
-                        isRequired
-                        onKeyDown={(e) => e.preventDefault()}
-                        value={stage.timeSpan}
-                        isInvalid={stage.timeSpan ? false : true}
-                        errorMessage="Số ngày thực hiện không được để trống"
-                        onValueChange={(event) => onStageChange(event, "timeSpan", stage.id || "")}
-                      />
+                      <div className="col-span-2">
+                        <div className="w-full grid grid-cols-3 gap-4">
+                          <Input
+                            className="mb-5"
+                            type="text"
+                            radius="sm"
+                            size="lg"
+                            label="Tên giai đoạn"
+                            placeholder="Nhập tên giai đoạn"
+                            labelPlacement="outside"
+                            isRequired
+                            value={stage.title}
+                            isInvalid={stage.title ? false : true}
+                            errorMessage="Tên giai đoạn không được để trống"
+                            onValueChange={(event) =>
+                              onStageChange(event, "title", stage.id || "")
+                            }
+                          />
+                          <DatePicker
+                            className="mb-5"
+                            radius="sm"
+                            size="lg"
+                            label="Ngày tiêm"
+                            value={
+                              stage.applyStageTime
+                                ? parseDate(stage.applyStageTime)
+                                : undefined
+                            }
+                            isDateUnavailable={(date: DateValue) =>
+                              stages.some(
+                                (x: VaccinationStageProps) =>
+                                  x.applyStageTime === date.toString() &&
+                                  x.id !== stage.id
+                              )
+                            }
+                            minValue={date.start}
+                            maxValue={date.end}
+                            labelPlacement="outside"
+                            isRequired
+                            isInvalid={stage.applyStageTime ? false : true}
+                            errorMessage="Ngày tiêm không được để trống"
+                            onChange={(event) =>
+                              onStageChange(
+                                event.toString(),
+                                "applyStageTime",
+                                stage.id || ""
+                              )
+                            }
+                          />
+                          <Input
+                            className="mb-5"
+                            type="number"
+                            min={1}
+                            max={50}
+                            radius="sm"
+                            size="lg"
+                            label="Số ngày thực hiện (dự kiến)"
+                            placeholder="Nhập số ngày thực hiện (dự kiến)"
+                            labelPlacement="outside"
+                            isRequired
+                            onKeyDown={(e) => e.preventDefault()}
+                            value={stage.timeSpan}
+                            isInvalid={stage.timeSpan ? false : true}
+                            errorMessage="Số ngày thực hiện không được để trống"
+                            onValueChange={(event) =>
+                              onStageChange(event, "timeSpan", stage.id || "")
+                            }
+                          />
+                        </div>
+                      </div>
+                      {/* todo */}
+                      <div className="">
+                        <label className="mb-2">
+                          <span>Các bước cần thực hiện</span>
+                        </label>
+                        {stage.vaccinationToDos?.map(
+                          (
+                            vacinationTodo: { description: string },
+                            index: number
+                          ) => {
+                            return (
+                              <div className="grid grid-cols-5 gap-2">
+                                <Input
+                                  className="mb-5 col-span-4"
+                                  type="text"
+                                  radius="sm"
+                                  size="lg"
+                                  label={`Bước ${index + 1}`}
+                                  labelPlacement="inside"
+                                  onKeyDown={(e) => e.preventDefault()}
+                                  value={vacinationTodo.description}
+                                />
+
+                              </div>
+                            );
+                          }
+                        )}
+                      </div>
                     </div>
                     <div className="flex flex-row items-start">
                       <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                        {stages.length > 1 && <Trash2Icon onClick={() => onDeleteStage(stage)} />}
+                        {stages.length > 1 && (
+                          <Trash2Icon onClick={() => onDeleteStage(stage)} />
+                        )}
                       </span>
                     </div>
                   </div>
@@ -349,13 +435,17 @@ const FirstVaccinationStep = ({ setStep, setVaccinationPlanFirstStepResult }: an
           </Card>
           <Card>
             <CardBody>
-              <p className="text-2xl font-semibold">Chọn heo cho kế hoạch tiêm phòng</p>
+              <p className="text-2xl font-semibold">
+                Chọn heo cho kế hoạch tiêm phòng
+              </p>
               <div className="mt-2 grid grid-cols-2 gap-4">
                 <div>
                   <Card className="mt-2" radius="sm">
                     <CardBody>
                       <div className="mb-1 flex justify-between">
-                        <p className="text-lg">Chọn heo theo {openBy === "cage" ? "Chuồng" : "Đàn"}</p>
+                        <p className="text-lg">
+                          Chọn heo theo {openBy === "cage" ? "Chuồng" : "Đàn"}
+                        </p>
                         <Popover key="select" placement="bottom">
                           <PopoverTrigger>
                             <Button isIconOnly color="primary" size="sm">
@@ -364,10 +454,27 @@ const FirstVaccinationStep = ({ setStep, setVaccinationPlanFirstStepResult }: an
                           </PopoverTrigger>
                           <PopoverContent>
                             <div className="flex flex-col px-1 py-2">
-                              <Button className="mb-2" color="primary" variant="solid" isDisabled={false} size="sm" onClick={() => onOpenSelectedPigsByHerdCage("herd")}>
+                              <Button
+                                className="mb-2"
+                                color="primary"
+                                variant="solid"
+                                isDisabled={false}
+                                size="sm"
+                                onClick={() =>
+                                  onOpenSelectedPigsByHerdCage("herd")
+                                }
+                              >
                                 <p className="text-white">Chọn theo đàn</p>
                               </Button>
-                              <Button color="primary" variant="solid" isDisabled={false} size="sm" onClick={() => onOpenSelectedPigsByHerdCage("cage")}>
+                              <Button
+                                color="primary"
+                                variant="solid"
+                                isDisabled={false}
+                                size="sm"
+                                onClick={() =>
+                                  onOpenSelectedPigsByHerdCage("cage")
+                                }
+                              >
                                 <p className="text-white">Chọn theo chuồng</p>
                               </Button>
                             </div>
@@ -375,7 +482,11 @@ const FirstVaccinationStep = ({ setStep, setVaccinationPlanFirstStepResult }: an
                         </Popover>
                       </div>
                       <Divider orientation="horizontal" className="my-2 b-2" />
-                      {openBy === "cage" ? <CageListReadOnly setSelected={setSelectedCages} /> : <HerdListReadOnly setSelected={setSelectedHerds} />}
+                      {openBy === "cage" ? (
+                        <CageListReadOnly setSelected={setSelectedCages} />
+                      ) : (
+                        <HerdListReadOnly setSelected={setSelectedHerds} />
+                      )}
                     </CardBody>
                   </Card>
                 </div>
