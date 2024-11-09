112fec1b3ac3e4f0d5e48bc2fdd65eb2723d4f92
nguyen.quang.huy <nguyen.quang.huy@vina-payroll.com>
2024-11-09 09:11:26 +0700

    change component name

--- src/app/(management)/veterinarian/vaccination/create-plan/page.tsx
+++ src/app/(management)/veterinarian/vaccination/create-plan/page.tsx
@@ -1,22 +1,15 @@
 "use client";
 import React from "react";
-import FirstVaccinationStep from "./_components/first-vaccination-step";
-import SecondVaccinationStep from "./_components/second-vaccination-step";
+import FirstVaccinationStep from "./_components/create-vaccination";
 import { useRouter } from "next/navigation";
 import { motion } from "framer-motion";
+import CreateVaccination from "./_components/create-vaccination";
 
 const CreatePlan = () => {
   const router = useRouter();
-  const [step, setStep] = React.useState<number>(1);
-  const [vaccinationPlanFirstStepResult, setVaccinationPlanFirstStepResult] = React.useState<any>();
-  React.useEffect(() => {
-    if (step === 3) {
-      router.push("/veterinarian/vaccination");
-    }
-  }, [step]);
   return (
     <motion.div initial={{ x: "100%" }} animate={{ x: 0 }} exit={{ x: "100%" }} transition={{ duration: 0.4 }}>
-      {step === 1 ? <FirstVaccinationStep /> : <SecondVaccinationStep setStep={setStep} vaccinationPlan={vaccinationPlanFirstStepResult} />}
+      <CreateVaccination />
     </motion.div>
   );
 };
