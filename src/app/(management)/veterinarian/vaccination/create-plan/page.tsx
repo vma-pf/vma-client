103602e3eacac8abde20e3ea50b8e91aa4b212d5
nguyen.quang.huy <nguyen.quang.huy@vina-payroll.com>
2024-11-10 13:06:07 +0700

    add select first data stage

--- src/app/(management)/veterinarian/vaccination/create-plan/page.tsx
+++ src/app/(management)/veterinarian/vaccination/create-plan/page.tsx
@@ -1,15 +1,46 @@
 "use client";
 import React from "react";
-import FirstVaccinationStep from "./_components/create-vaccination";
 import { useRouter } from "next/navigation";
 import { motion } from "framer-motion";
 import CreateVaccination from "./_components/create-vaccination";
+import { Tab, Tabs } from "@nextui-org/react";
+import { TbVaccine } from "react-icons/tb";
+import { ClipboardCheck } from "lucide-react";
 
 const CreatePlan = () => {
-  const router = useRouter();
   return (
-    <motion.div initial={{ x: "100%" }} animate={{ x: 0 }} exit={{ x: "100%" }} transition={{ duration: 0.4 }}>
-      <CreateVaccination />
+    <motion.div
+      initial={{ x: "100%" }}
+      animate={{ x: 0 }}
+      exit={{ x: "100%" }}
+      transition={{ duration: 0.4 }}
+    >
+      <Tabs size="lg" color="primary" variant="solid" defaultSelectedKey="1">
+        <Tab
+          key="1"
+          title={
+            <div className="flex items-center">
+              <TbVaccine size={20} />
+              <span className="ml-2">Tạo lịch tiêm phòng</span>
+            </div>
+          }
+        >
+          <CreateVaccination />
+        </Tab>
+        <Tab
+          key="2"
+          title={
+            <div className="flex items-center">
+              <ClipboardCheck size={20} />
+              <span className="ml-2">Mẫu lịch tiêm phòng</span>
+            </div>
+          }
+        >
+          <div>
+            asc
+          </div>
+        </Tab>
+      </Tabs>
     </motion.div>
   );
 };
