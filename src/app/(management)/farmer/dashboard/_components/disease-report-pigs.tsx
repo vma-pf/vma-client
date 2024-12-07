4b92b3eb94c5e832d939ded3a6fe66732b58ac89
dat <tranminhdat0805@gmail.com>
2024-12-07 12:15:47 +0700

    fix: add date picker to choose month

--- src/app/(management)/farmer/dashboard/_components/disease-report-pigs.tsx
+++ src/app/(management)/farmer/dashboard/_components/disease-report-pigs.tsx
@@ -1,4 +1,5 @@
-import { Button, Spinner } from "@nextui-org/react";
+import { parseDate } from "@internationalized/date";
+import { Button, DatePicker, DateValue, Skeleton, Spinner } from "@nextui-org/react";
 import { ResponseObjectList } from "@oursrc/lib/models/response-object";
 import { diseaseReportService } from "@oursrc/lib/services/diseaseReportService";
 import React from "react";
@@ -10,15 +11,19 @@ type DiseaseReportPigs = {
 };
 
 const DiseaseReportPigs = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
   const [totalPages, setTotalPages] = React.useState(1);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const [loadMore, setLoadMore] = React.useState(true);
   const [hasMore, setHasMore] = React.useState(true);
   const [diseaseReportPigs, setDiseaseReportPigs] = React.useState<DiseaseReportPigs[]>([]);
+  const [date, setDate] = React.useState<DateValue>(parseDate(new Date().toISOString().split("T")[0]));
+
   const fetchData = async () => {
     try {
+      setIsLoading(true);
       setLoadMore(true);
-      const res: ResponseObjectList<DiseaseReportPigs> = await diseaseReportService.getDiseaseReportPigs(1, 1, 10);
+      const res: ResponseObjectList<DiseaseReportPigs> = await diseaseReportService.getDiseaseReportPigs(date.month ?? "", 1, 10);
       if (res.isSuccess) {
         setDiseaseReportPigs(res.data.data);
         setTotalPages(res.data.totalPages);
@@ -30,12 +35,13 @@ const DiseaseReportPigs = () => {
     } catch (e) {
       console.log(e);
     } finally {
+      setIsLoading(false);
       setLoadMore(false);
     }
   };
   React.useEffect(() => {
     fetchData();
-  }, [rowsPerPage]);
+  }, [rowsPerPage, date]);
   return (
     <div>
       <div className="flex justify-between items-center">
@@ -45,22 +51,46 @@ const DiseaseReportPigs = () => {
         <div className="text-center">Không có bệnh</div>
       ) : (
         <div>
-          {diseaseReportPigs.map((item, index) => (
-            <div key={index} className="mx-2 my-3 hover:bg-gray-100 dark:hover:bg-zinc-600 p-2 rounded-lg">
-              <p className="text-lg font-semibold">{item.diseaseName ?? "Tên bệnh"}</p>
-              <p className="text-sm">
-                Số heo bị bệnh: <span>{item.numberOfPigs ?? 0} con</span>
-              </p>
+          <DatePicker
+            showMonthAndYearPickers
+            className="mb-4 mx-auto w-5/6"
+            label="Chọn ngày"
+            labelPlacement="outside"
+            value={date}
+            onChange={(value) => {
+              setDate(value);
+            }}
+          />
+          {isLoading ? (
+            <div className="mx-7 my-3 grid grid-cols-1 gap-6">
+              {Array.from({ length: 5 }).map((_, index) => (
+                <div key={index} className="m-2 border-2 rounded-lg">
+                  <Skeleton className="rounded-lg">
+                    <div className="h-28 rounded-lg bg-default-300"></div>
+                  </Skeleton>
+                </div>
+              ))}
             </div>
-          ))}
-          {hasMore && (
-            <div className="flex justify-center">
-              {loadMore ? (
-                <Spinner color="primary" />
-              ) : (
-                <Button className="w-full" variant="light" size="sm" color="default" onPress={() => setRowsPerPage(rowsPerPage + 5)}>
-                  Xem thêm
-                </Button>
+          ) : (
+            <div>
+              {diseaseReportPigs.map((item, index) => (
+                <div key={index} className="mx-2 my-3 hover:bg-gray-100 dark:hover:bg-zinc-600 p-2 rounded-lg">
+                  <p className="text-lg font-semibold">{item.diseaseName ?? "Tên bệnh"}</p>
+                  <p className="text-sm">
+                    Số heo bị bệnh: <span>{item.numberOfPigs ?? 0} con</span>
+                  </p>
+                </div>
+              ))}
+              {hasMore && (
+                <div className="flex justify-center">
+                  {loadMore ? (
+                    <Spinner color="primary" />
+                  ) : (
+                    <Button className="w-full" variant="light" size="sm" color="default" onPress={() => setRowsPerPage(rowsPerPage + 5)}>
+                      Xem thêm
+                    </Button>
+                  )}
+                </div>
               )}
             </div>
           )}
